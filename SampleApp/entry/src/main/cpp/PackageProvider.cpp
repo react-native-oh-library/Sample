e3552ead29b428ba771c3c28d683cfe2c45e668d
林嘉诚 <425765923@qq.com>
2024-02-07 14:47:53 +0800

    新增maskview,删除布局多余的margin

--- SampleApp/entry/src/main/cpp/PackageProvider.cpp
+++ SampleApp/entry/src/main/cpp/PackageProvider.cpp
@@ -4,11 +4,15 @@
 #include "SVGPackage.h"
 #include "SafeAreaViewPackage.h"
 #include "GestureHandlerPackage.h"
+#include "MaskedPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-    return {std::make_shared<SampleTurboModulePackage>(ctx), std::make_shared<ViewPagerPackage>(ctx),
-            std::make_shared<SVGPackage>(ctx), std::make_shared<SafeAreaViewPackage>(ctx),
-            std::make_shared<GestureHandlerPackage>(ctx)};
+    return {std::make_shared<SampleTurboModulePackage>(ctx),
+            std::make_shared<ViewPagerPackage>(ctx),
+            std::make_shared<SVGPackage>(ctx),
+            std::make_shared<SafeAreaViewPackage>(ctx),
+            std::make_shared<GestureHandlerPackage>(ctx),
+            std::make_shared<MaskedPackage>(ctx)};
 }
